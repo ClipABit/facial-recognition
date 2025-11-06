d9a4a96e2d5768778f26bb15572dcdd9860e8e48
Yifan Zhang <yifanzhang0905@gmail.com>
2025-11-06 01:43:20 -0500

    implement function to plot the embedding & cluster

---
+++ cluster_plot.py
@@ -0,0 +1,42 @@
+# given photos, detect and embed faces, then cluster them
+# generate the plot of the clustering
+import matplotlib.pyplot as plt
+from sklearn.manifold import TSNE
+import matplotlib.colors as mcolors
+import matplotlib.ticker as mticker
+from utils import *
+
+def plot_cluster(embeddings, clustering):
+    # reduce embedding dimensions to 2D for plotting
+    reducer = TSNE(n_components=2, perplexity=5)
+    points_2d = reducer.fit_transform(np.array(embeddings))
+    cluster_num = max(clustering)+1
+
+    # plot
+    plt.figure(figsize=(8, 6))
+    scatter = plt.scatter(
+        points_2d[:, 0],
+        points_2d[:, 1],
+        c=clustering,
+        cmap=plt.get_cmap('tab10', cluster_num),
+        norm=mcolors.BoundaryNorm(np.arange(0, cluster_num + 1) - 0.5, cluster_num),
+        s=30
+    )
+    tick_locs = np.arange(0, cluster_num)
+    cbar = plt.colorbar(scatter, ticks=tick_locs, label="Cluster ID")
+    cbar.formatter = mticker.FormatStrFormatter('%d')
+    cbar.update_ticks()
+    plt.title("face embedding cluster")
+    plt.show()
+
+
+embeddings = []
+
+## replace "photos/photo1.jpg" with path to any photo
+faces = detect_and_embed("photos/photo1.jpg")
+
+for f in faces:
+    embeddings.append(f["embedding"])
+clustering = cluster(embeddings)
+# print(clustering)
+plot_cluster(embeddings, clustering)
